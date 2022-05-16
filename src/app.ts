6f0d3c3c2d25f012b8d08030d43969a6483c594b
vitor <vitorsbsilva@gmail.com>
2022-05-16 13:08:26 -0300

    create validation middleware to POST /users

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import ProductsController from './controllers/products.controller';
 import { nameValidation, amountValidation } from './middlewares/products.middlewares';
+import UserValidation from './middlewares/users.middlewares';
 
 const app = express();
 export const router = Router();
@@ -9,9 +10,17 @@ app.use(express.json());
 app.use(router);
 
 const productsController = new ProductsController();
+const userVal = new UserValidation();
 
 router.get('/products', productsController.getAll);
 router.post('/products', nameValidation, amountValidation, productsController.create);
-router.post('/users');
+
+router.post(
+  '/users', 
+  userVal.usernameValidation, 
+  userVal.classeValidation,
+  userVal.levelValidation,
+  userVal.passwordValidation,
+);
 
 export default app;
